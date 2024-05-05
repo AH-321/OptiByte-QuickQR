15792c1d88aac5cbfb7d67f13290c1ab1d468e56
AH-321 <116464553+AH-321@users.noreply.github.com>
2024-05-04 22:05:19 -0700

    Update encode.py

--- scripts/encode.py
+++ scripts/encode.py
@@ -1,1 +1,31 @@
+import os
+import qrcode
 
+python_interpreter = '.venv\\Scripts\\python.exe'
+
+def generate_qr_code(data, file_name):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(file_name)
+
+def new_entry():
+    print("#####NEW ENTRY#####")
+    encode_data = input("Enter name:")
+    directory = "saved_codes"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    code_file = os.path.join(directory, encode_data + ".png")
+    generate_qr_code(encode_data, code_file)
+    print(f"QR code generated and saved as {code_file}")
+
+    os.system(f'{python_interpreter} main.py')
+
+new_entry()
